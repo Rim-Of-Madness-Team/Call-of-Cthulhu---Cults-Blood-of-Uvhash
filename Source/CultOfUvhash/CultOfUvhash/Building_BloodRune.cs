55aa33e246c0aff28c331261da1013893cccd336
jecrell <matt.walls31@gmail.com>
2017-04-09 13:40:11 +0900

    Bones added
    Boned

---
+++ Source/CultOfUvhash/CultOfUvhash/Building_BloodRune.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace CultOfUvhash
+{
+    public class Building_BloodRune : Building
+    {
+        public Building_BloodHub FindNearestHub()
+        {
+            Building_BloodHub parentCandidate = null;
+
+            Map map = this.MapHeld;
+            if (map != null)
+            {
+                IEnumerable<Building_BloodHub> bloodHubs = map.listerBuildings.AllBuildingsColonistOfClass<Building_BloodHub>();
+                if (bloodHubs != null)
+                {
+                    if (this.Position != null)
+                    {
+                        float num = -1;
+                        foreach (Building_BloodHub hub in bloodHubs)
+                        {
+                            if (hub.Position != null)
+                            {
+                                float num2 = hub.Position.DistanceToSquared(this.Position);
+                                if (num2 < num || parentCandidate == null)
+                                {
+                                    parentCandidate = hub;
+                                    num = num2;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return parentCandidate;
+        }
+
+        public override void SpawnSetup(Map map)
+        {
+            base.SpawnSetup(map);
+        }
+    }
+}
---
+++ Source/CultOfUvhash/CultOfUvhash/HarmonyPatches.cs
@@ -0,0 +1,47 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using System.Reflection;
+using UnityEngine;
+
+namespace CompActivatableEffect
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.jecrell.uvhash");
+            harmony.Patch(typeof(Pawn).GetMethod("ButcherProducts"), null, new HarmonyMethod(typeof(HarmonyPatches).GetMethod("ButcherProducts_PostFix")));
+        }
+
+        public static readonly float boneDivisor = 2f;
+
+        // Verse.Pawn
+        public static void ButcherProducts_PostFix(Pawn __instance, Pawn butcher, float efficiency)
+        {
+            //Log.Message("1");
+            if (__instance.RaceProps.meatDef != null)
+            {
+
+                //Log.Message("2");
+                int boneCount = GenMath.RoundRandom((__instance.GetStatValue(StatDefOf.MeatAmount, true) * efficiency) / boneDivisor);
+                if (boneCount > 0)
+                {
+
+                    //Log.Message("3");
+                    Thing bone = ThingMaker.MakeThing(ThingDef.Named("Uvhash_Bone"), null);
+                    bone.stackCount = boneCount;
+                    GenPlace.TryPlaceThing(bone, butcher.PositionHeld, butcher.MapHeld, ThingPlaceMode.Near);
+                    //__result.Add(meat);
+                }
+            }
+        }
+
+    }
+}
